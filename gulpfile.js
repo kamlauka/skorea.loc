a74c66d7ee64ba66204d9ef6194bf9afe9501d88
kamlauka <kamlauka@gmail.com>
2018-08-24 15:10:46 +0300

    front fixed bugs

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,37 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var browserSync = require('browser-sync');
-var reload = browserSync.reload;
-// var cssnano = require('gulp-cssnano'); // Подключаем пакет для минификации CSS
-// var rename = require('gulp-rename');
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var plumber = require("gulp-plumber");
+var server = require("browser-sync").create();
 
 
-gulp.task('default', function () {
-    return gulp.src('scss/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        // .pipe(cssnano()) // Сжимаем
-        // .pipe(rename({suffix: '.min'})) // Добавляем суффикс .min
-        .pipe(gulp.dest('css'))
-
-        .pipe(reload({
-            stream: true
-        }))
+gulp.task("sass", function() {
+    return gulp.src("scss/*.scss")
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(gulp.dest("css"))
+        .pipe(server.stream());
 });
 
-gulp.task('browserSync', function() {
-    browserSync({
+
+
+gulp.task("serve", gulp.series(["sass"], function () {
+    server.init({
         server: {
             baseDir: '.'
         },
-        tunnel: true,
-        host: 'skoreya.loc',
-        port: 'http://skoreya.loc/',
-        logPrefix: "My_Local_Server"
-    })
-});
+        notify: false,
+        open: true,
+        cors: true,
+        ui: false
+    });
 
-gulp.task('watch', ['browserSync', 'default'], function (){
-    gulp.watch('public_html/template/src/scss/*.scss', ['default']);
-    gulp.watch('public_html/template/dist/prizes.html', reload);
-});
+    gulp.watch("scss/*.scss", gulp.parallel(["sass"]));
+    gulp.watch("*.html").on("change", server.reload);
+}));
+
+
+
+gulp.task('def', gulp.series('sass', 'serve'));
+
+
+
